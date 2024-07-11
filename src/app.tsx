4f8ba0c53dfccdb6628b1513a9d187ee8fa901c9
MatheusVinagre <matheus.vinagre2@gmail.com>
2024-07-11 13:40:28 -0300

    Better Modal Screen

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, AtSign, Plus, X} from 'lucide-react'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 export function App() {
 
   const [planConfirmed, setPlanConfirmed] = useState(false)
   const [modalScreen, setModalScreen] = useState(false)
+
+  const [emailsToInvite, setEmailsToInvite] = useState([''])
 
   function ConfirmedPlaceDate(){
     setPlanConfirmed(true)
@@ -19,6 +21,32 @@ export function App() {
 
   function closeModalScreen(){
     setModalScreen(false)
+  }
+
+  function addEmailToInvite(event: FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    //setEmailsToInvite([...emailsToInvite, newEmail])
+    const data = new FormData(event.currentTarget)
+    const newEmail = data.get('email')?.toString()
+    
+
+    if(!newEmail){
+      return
+    }
+
+    if(emailsToInvite.includes(newEmail)){
+      event.currentTarget.reset()
+      return
+    }
+
+    setEmailsToInvite([...emailsToInvite, newEmail])
+    event.currentTarget.reset()
+  }
+
+  function removeEmailToInvite(emailToRemove: string){
+    const newList = emailsToInvite.filter(email => email != emailToRemove)
+
+    setEmailsToInvite(newList)
   }
 
   return (
@@ -77,14 +105,19 @@ export function App() {
             </div>
             <p className="text-zinc-400 text-sm">Os convidados irão receber emails para confirmar a participação na viagem.</p>
             <div className="flex flex-wrap gap-2">
-              <div className="flex bg-zinc-800 gap-2 rounded-md p-2 items-center">
-                <p className="text-zinc-300 text-sm">Emails@gmails.com</p><X className="size-4"/>
-              </div>
+              {emailsToInvite.map(email => {
+                return (
+                  <div key={email} className="flex bg-zinc-800 gap-2 rounded-md p-2 items-center">
+                    <p className="text-zinc-300 text-sm">{email}</p><button onClick={() => removeEmailToInvite(email)}><X className="size-4"/></button>
+                  </div>
+                )
+              })}
+              
             </div>
             <div className="h-px w-full bg-zinc-800" />
-            <form className=" bg-black rounded-lg flex items-center gap-2 w-full border p-2.5">
+            <form onSubmit={addEmailToInvite} className=" bg-black rounded-lg flex items-center gap-2 w-full border p-2.5">
               <AtSign className="size-5 text-zinc-400"/>
-              <input type="text" placeholder="Digite o email do convidado" className="bg-transparent text-lg placeholder-zinc-400 flex-1 outline-none"/>
+              <input type="email" name="email" placeholder="Digite o email do convidado" className="bg-transparent text-lg placeholder-zinc-400 flex-1 outline-none"/>
               <button className="bg-lime-300 text-lime-950 rounded-lg p-3 flex font-medium items-center gap-1 hover:bg-yellow-400">
                     Convidar <Plus className="size-5"/>
               </button>
