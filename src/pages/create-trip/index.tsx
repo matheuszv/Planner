fbf357fb8ccc4c87870ad70434fd6bf2daa2f630
Matheus Vinagre <matheus.vinagre@hotmail.com>
2024-07-23 22:15:19 -0300

    move bg

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -136,7 +136,7 @@ export function CreateTrip() {
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
       <div className="max-w-3xl w-full px-6 text-center space-y-10">
         <div className="flex flex-col items-center gap-2">
-          <img src="./Logo.svg" alt="Logo Planner" />
+          <img src="./assets/Logo.svg" alt="Logo Planner" />
           <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua próxima viagem!</p>
         </div>
 
